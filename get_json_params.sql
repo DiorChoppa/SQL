b2799aed3598fc303355bbe1e3249c4a4e03a95e
DiorChoppa <78878912+DiorChoppa@users.noreply.github.com>
2021-10-22 11:35:06 +0300

    Update get_json_params.sql

--- get_json_params.sql
+++ get_json_params.sql
@@ -1,5 +1,5 @@
---COPY (SELECT distinct json_object_keys((content#>> '{}')::json) FROM vpct_chapters ORDER BY json_object_keys((content#>> '{}')::json) ASC) TO 'C:\Users\kdvoryadkin\Desktop\json.txt'
+--COPY (SELECT distinct json_object_keys((content#>> '{}')::json) FROM @NAME@ ORDER BY json_object_keys((content#>> '{}')::json) ASC) TO '..\json.txt'
 SELECT json_object_keys((content#>> '{}')::json),
-COUNT(*) FROM vpct_chapters
+COUNT(*) FROM @NAME@
 GROUP BY json_object_keys((content#>> '{}')::json)
 ORDER BY count DESC
